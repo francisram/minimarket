23e78cdecb1d220d44c9f32dddbc6c006a468684
Francis Lopez <francisrla87@gmail.com>
2025-09-28 15:11:23 -0300

    primer commit

---
+++ src/test/java/com/sanmartin/minimarketbase/MinimarketbaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanmartin.minimarketbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinimarketbaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
